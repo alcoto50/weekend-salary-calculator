06fff530d06d74a4278e4fb17186c2d13e9a0d0a
al cotton <alvicoto@gmail.com>
2024-11-03 10:53:45 -0600

    ase enter the commit message for your changes. Lines starting

---
+++ style.js
@@ -0,0 +1,20 @@
+// This is a new JavaScript file for weekend-salary-calculator
+// and will assign input elements that are inputed by the user,
+
+let firstNameInputElement = document.querySelector("#firstNameInput.value");
+console.log(firstNameInputElement);
+
+ let lastNameInputElement = Document.querySelector("#lastNameInput.value");
+ console.log(lastNameInputElement);
+
+let IdInputElement = Document.querySelector("#IdInput.value");
+    console.log(IdInputElement);
+
+let titleInputElement = Document.querySelector("#titleInput.value");
+    console.log(hoursWorkedInputElement);
+
+let annualSalaryInputElement = Document.querySelector("#annualSalaryInput.value");
+    console.log(annualSalaryInputElement);
+
+
+  
